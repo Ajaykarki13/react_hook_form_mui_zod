14524a427a4446c4a8c94276c8cf1dcf90730927
Ajay <ajaymum698@gmail.com>
2024-06-14 16:22:38 +0530

    new  files added

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const patterns = {email: /^([a-zA-Z0-9_\-\+\.]+)@([a-zA-Z\-]+[.])([a-zA-Z]{2,})+$/}
---
+++ src/users/types/schem.ts
@@ -0,0 +1,10 @@
+import {z} from 'zod';
+import { patterns } from '../../constants';
+
+export const schema = z.object({
+    name:z.string().min(1,{message:'Required'}),
+    email: z
+    .string()
+    .min(1, {message:'email is required'})
+    .refine((text)=> patterns.email.test(text), {message:'Email invalid'}),
+})
