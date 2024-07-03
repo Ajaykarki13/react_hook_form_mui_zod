79efb4ae3db57d8832f9fd84e4762a0d99ade600
Ajay <ajaymum698@gmail.com>
2024-07-03 13:35:17 +0530

    added zod,schema

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,4 +1,29 @@
 
+import {useForm} from 'react-hook-form';
+import {Stack, TextField} from '@mui/material';
+import { Schema ,schema} from '../types/schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+
 export function Users(){
-    return <div>users.........</div>
-}
+    const {register,formState:{errors}} = useForm<Schema>({mode:'all',
+                                     resolver: zodResolver(schema)});
+
+    return( 
+    <Stack sx={{gap:2,width:600 }}>
+
+    <TextField 
+    {...register('name')} 
+    label="Name"
+     error={!!errors.name}
+     helperText={errors.name?.message}
+     />
+
+    <TextField 
+    {...register('email')} 
+    label="Email"
+    error={!!errors.email}
+    helperText={errors.email?.message}
+    />
+
+    </Stack>
+)}  
--- src/users/types/schema.ts
+++ src/users/types/schema.ts
@@ -7,4 +7,7 @@ export const schema = z.object({
     .string()
     .min(1, {message:'email is required'})
     .refine((text)=> patterns.email.test(text), {message:'Email invalid'}),
+
 })
+
+export type Schema = z.infer<typeof schema>
